54ba179d688e81639425da0d189f9c5f0b70aea1
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-30 18:50:55 +0530

    Checked if recognition result is final

--- script.js
+++ script.js
@@ -29,3 +29,4 @@ function processResult(results){
     for(let i = 0; i < results.length; i++){
         let transcript = results[i][0].transcript;
         transcript.replace("\n","<br>");
+        if(results[i].isFinal){
